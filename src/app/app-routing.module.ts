57cdc6ae383f25b0f8b18c315ad20e0dca740069
Jose Vicente <jantvb@gmail.com>
2021-08-17 16:56:52 -0400

    Util

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { IntegrationComponent } from './components/integration/integration.component';
 
 const routes: Routes = [];
 
