692167d763fd432f8a2104218b11ca7734fb031d
Jose Vicente <jantvb@gmail.com>
2021-08-22 13:43:13 -0400

    Menu

--- src/app/util/util.ts
+++ src/app/util/util.ts
@@ -10,10 +10,10 @@ export function mapMenu(integrations: Array<Integration>): Array<Menu> {
 }
 
 function generateAll(integrations: Array<Integration>): Menu {
-  const allMenu: Menu = new Menu();
-  allMenu.name = 'all';
-  allMenu.display = 'All'
-  allMenu.icon = 'apps';
+  const allMenu: Menu  = new Menu();
+  allMenu.name         = 'all';
+  allMenu.display      = 'All'
+  allMenu.icon         = 'apps';
   allMenu.integrations = new Array<Integration>();
   integrations.forEach(i => {
     allMenu.integrations.push(i);
@@ -38,11 +38,11 @@ function generateMenus(integrations: Array<Integration>): Array<Menu> {
 
 // SALES
 function generateSales(integrations: Array<Integration>): Menu {
-  const saleMenu: Menu = new Menu();
-  saleMenu.name = 'sales';
-  saleMenu.display = 'Sales';
-  saleMenu.icon = 'store';
-  saleMenu.subMenus = subMenuSales(integrations);
+  const saleMenu: Menu  = new Menu();
+  saleMenu.name         = 'sales';
+  saleMenu.display      = 'Sales';
+  saleMenu.icon         = 'store';
+  saleMenu.subMenus     = subMenuSales(integrations);
   saleMenu.integrations = new Array<Integration>();
   integrations.forEach(i => {
     i.tags.forEach(t => {
@@ -55,12 +55,12 @@ function generateSales(integrations: Array<Integration>): Menu {
 }
 
 function subMenuSales(integrations: Array<Integration>): Array<SubMenu> {
-  const sm: Array<SubMenu> = new Array<SubMenu>();
+  const sm: Array<SubMenu>     = new Array<SubMenu>();
   // Online Order
   const smOnlineOrder: SubMenu = new SubMenu();
-  smOnlineOrder.name = 'online_order';
-  smOnlineOrder.display = 'Online Order';
-  smOnlineOrder.integrations = new Array<Integration>();
+  smOnlineOrder.name           = 'online_order';
+  smOnlineOrder.display        = 'Online Order';
+  smOnlineOrder.integrations   = new Array<Integration>();
   integrations.forEach(i => {
     i.tags.forEach(t => {
       if (smOnlineOrder.name === t) {
@@ -71,9 +71,9 @@ function subMenuSales(integrations: Array<Integration>): Array<SubMenu> {
   sm.push(smOnlineOrder);
   // Delivery
   const smDelivery: SubMenu = new SubMenu();
-  smDelivery.name = 'delivery';
-  smDelivery.display = 'Delivery';
-  smDelivery.integrations = new Array<Integration>();
+  smDelivery.name           = 'delivery';
+  smDelivery.display        = 'Delivery';
+  smDelivery.integrations   = new Array<Integration>();
   integrations.forEach(i => {
     i.tags.forEach(t => {
       if (smDelivery.name === t) {
@@ -88,11 +88,11 @@ function subMenuSales(integrations: Array<Integration>): Array<SubMenu> {
 
 // MARKETING
 function generateMarketing(integrations: Array<Integration>): Menu {
-  const marketingMenu: Menu = new Menu();
-  marketingMenu.name = 'marketing';
-  marketingMenu.display = 'Marketing';
-  marketingMenu.icon = 'shopping_bag';
-  marketingMenu.subMenus = subMenuMarketing(integrations);
+  const marketingMenu: Menu  = new Menu();
+  marketingMenu.name         = 'marketing';
+  marketingMenu.display      = 'Marketing';
+  marketingMenu.icon         = 'shopping_bag';
+  marketingMenu.subMenus     = subMenuMarketing(integrations);
   marketingMenu.integrations = new Array<Integration>();
   integrations.forEach(i => {
     i.tags.forEach(t => {
@@ -108,10 +108,10 @@ function generateMarketing(integrations: Array<Integration>): Menu {
 function subMenuMarketing(integrations: Array<Integration>): Array<SubMenu> {
   const sm: Array<SubMenu> = new Array<SubMenu>();
   // Email
-  const smEmail: SubMenu = new SubMenu();
-  smEmail.name = 'email';
-  smEmail.display = 'Email (i.e. MailChimp)';
-  smEmail.integrations = new Array<Integration>();
+  const smEmail: SubMenu   = new SubMenu();
+  smEmail.name             = 'email';
+  smEmail.display          = 'Email (i.e. MailChimp)';
+  smEmail.integrations     = new Array<Integration>();
   integrations.forEach(i => {
     i.tags.forEach(t => {
       if (smEmail.name === t) {
@@ -122,9 +122,9 @@ function subMenuMarketing(integrations: Array<Integration>): Array<SubMenu> {
   sm.push(smEmail);
   // Loyalty
   const smLoyalty: SubMenu = new SubMenu();
-  smLoyalty.name = 'loyalty';
-  smLoyalty.display = 'Loyalty';
-  smLoyalty.integrations = new Array<Integration>();
+  smLoyalty.name           = 'loyalty';
+  smLoyalty.display        = 'Loyalty';
+  smLoyalty.integrations   = new Array<Integration>();
   integrations.forEach(i => {
     i.tags.forEach(t => {
       if (smLoyalty.name === t) {
@@ -139,11 +139,11 @@ function subMenuMarketing(integrations: Array<Integration>): Array<SubMenu> {
 
 // LABOR
 function generateLabor(integrations: Array<Integration>): Menu {
-  const laborMenu: Menu = new Menu();
-  laborMenu.name = 'labor';
-  laborMenu.display = 'Labor';
-  laborMenu.icon = 'folder_open';
-  laborMenu.subMenus = subMenuLabor(integrations);
+  const laborMenu: Menu  = new Menu();
+  laborMenu.name         = 'labor';
+  laborMenu.display      = 'Labor';
+  laborMenu.icon         = 'folder_open';
+  laborMenu.subMenus     = subMenuLabor(integrations);
   laborMenu.integrations = new Array<Integration>();
   integrations.forEach(i => {
     i.tags.forEach(t => {
@@ -160,9 +160,9 @@ function subMenuLabor(integrations: Array<Integration>): Array<SubMenu> {
   const sm: Array<SubMenu> = new Array<SubMenu>();
   // Payroll
   const smPayroll: SubMenu = new SubMenu();
-  smPayroll.name = 'payroll';
-  smPayroll.display = 'Payroll';
-  smPayroll.integrations = new Array<Integration>();
+  smPayroll.name           = 'payroll';
+  smPayroll.display        = 'Payroll';
+  smPayroll.integrations   = new Array<Integration>();
   integrations.forEach(i => {
     i.tags.forEach(t => {
       if (smPayroll.name === t) {
@@ -173,9 +173,9 @@ function subMenuLabor(integrations: Array<Integration>): Array<SubMenu> {
   sm.push(smPayroll);
   // Attendance
   const smAttendance: SubMenu = new SubMenu();
-  smAttendance.name = 'attendance';
-  smAttendance.display = 'Attendance';
-  smAttendance.integrations = new Array<Integration>();
+  smAttendance.name           = 'attendance';
+  smAttendance.display        = 'Attendance';
+  smAttendance.integrations   = new Array<Integration>();
   integrations.forEach(i => {
     i.tags.forEach(t => {
       if (smAttendance.name === t) {
@@ -190,11 +190,11 @@ function subMenuLabor(integrations: Array<Integration>): Array<SubMenu> {
 
 // PAYMENTS
 function generatePayments(integrations: Array<Integration>): Menu {
-  const paymentsMenu: Menu = new Menu();
-  paymentsMenu.name = 'payments';
-  paymentsMenu.display = 'Payments';
-  paymentsMenu.icon = 'credit_score';
-  paymentsMenu.subMenus = subMenuPayments(integrations);
+  const paymentsMenu: Menu  = new Menu();
+  paymentsMenu.name         = 'payments';
+  paymentsMenu.display      = 'Payments';
+  paymentsMenu.icon         = 'credit_score';
+  paymentsMenu.subMenus     = subMenuPayments(integrations);
   paymentsMenu.integrations = new Array<Integration>();
   integrations.forEach(i => {
     i.tags.forEach(t => {
@@ -209,10 +209,10 @@ function generatePayments(integrations: Array<Integration>): Menu {
 function subMenuPayments(integrations: Array<Integration>): Array<SubMenu> {
   const sm: Array<SubMenu> = new Array<SubMenu>();
   // Gift
-  const smGift: SubMenu = new SubMenu();
-  smGift.name = 'gift';
-  smGift.display = 'Gift';
-  smGift.integrations = new Array<Integration>();
+  const smGift: SubMenu    = new SubMenu();
+  smGift.name              = 'gift';
+  smGift.display           = 'Gift';
+  smGift.integrations      = new Array<Integration>();
   integrations.forEach(i => {
     i.tags.forEach(t => {
       if (smGift.name === t) {
@@ -223,9 +223,9 @@ function subMenuPayments(integrations: Array<Integration>): Array<SubMenu> {
   sm.push(smGift);
   // EMV
   const smEMV: SubMenu = new SubMenu();
-  smEMV.name = 'emv';
-  smEMV.display = 'EMV';
-  smEMV.integrations = new Array<Integration>();
+  smEMV.name           = 'emv';
+  smEMV.display        = 'EMV';
+  smEMV.integrations   = new Array<Integration>();
   integrations.forEach(i => {
     i.tags.forEach(t => {
       if (smEMV.name === t) {
@@ -236,9 +236,9 @@ function subMenuPayments(integrations: Array<Integration>): Array<SubMenu> {
   sm.push(smEMV);
   // Swipe CC
   const smSwipeCC: SubMenu = new SubMenu();
-  smSwipeCC.name = 'swipe_cc';
-  smSwipeCC.display = 'Swipe CC';
-  smSwipeCC.integrations = new Array<Integration>();
+  smSwipeCC.name           = 'swipe_cc';
+  smSwipeCC.display        = 'Swipe CC';
+  smSwipeCC.integrations   = new Array<Integration>();
   integrations.forEach(i => {
     i.tags.forEach(t => {
       if (smSwipeCC.name === t) {
@@ -253,11 +253,11 @@ function subMenuPayments(integrations: Array<Integration>): Array<SubMenu> {
 
 // SECURITY
 function generateSecurity(integrations: Array<Integration>): Menu {
-  const securityMenu: Menu = new Menu();
-  securityMenu.name = 'security';
-  securityMenu.display = 'Security';
-  securityMenu.icon = 'shield';
-  securityMenu.subMenus = subMenuSecurity(integrations);
+  const securityMenu: Menu  = new Menu();
+  securityMenu.name         = 'security';
+  securityMenu.display      = 'Security';
+  securityMenu.icon         = 'shield';
+  securityMenu.subMenus     = subMenuSecurity(integrations);
   securityMenu.integrations = new Array<Integration>();
   integrations.forEach(i => {
     i.tags.forEach(t => {
@@ -272,10 +272,10 @@ function generateSecurity(integrations: Array<Integration>): Menu {
 function subMenuSecurity(integrations: Array<Integration>): Array<SubMenu> {
   const sm: Array<SubMenu> = new Array<SubMenu>();
   // Camera
-  const smCamera: SubMenu = new SubMenu();
-  smCamera.name = 'camera';
-  smCamera.display = 'Camera';
-  smCamera.integrations = new Array<Integration>();
+  const smCamera: SubMenu  = new SubMenu();
+  smCamera.name            = 'camera';
+  smCamera.display         = 'Camera';
+  smCamera.integrations    = new Array<Integration>();
   integrations.forEach(i => {
     i.tags.forEach(t => {
       if (smCamera.name === t) {
@@ -286,9 +286,9 @@ function subMenuSecurity(integrations: Array<Integration>): Array<SubMenu> {
   sm.push(smCamera);
   // Cash Management
   const smCashManagement: SubMenu = new SubMenu();
-  smCashManagement.name = 'cash_management';
-  smCashManagement.display = 'Cash Management';
-  smCashManagement.integrations = new Array<Integration>();
+  smCashManagement.name           = 'cash_management';
+  smCashManagement.display        = 'Cash Management';
+  smCashManagement.integrations   = new Array<Integration>();
   integrations.forEach(i => {
     i.tags.forEach(t => {
       if (smCashManagement.name === t) {
@@ -303,11 +303,11 @@ function subMenuSecurity(integrations: Array<Integration>): Array<SubMenu> {
 
 // COMMUNICATIONS
 function generateCommunications(integrations: Array<Integration>): Menu {
-  const communicationsMenu: Menu = new Menu();
-  communicationsMenu.name = 'communications';
-  communicationsMenu.display = 'Communications';
-  communicationsMenu.icon = 'sensors';
-  communicationsMenu.subMenus = subMenuCommunications(integrations);
+  const communicationsMenu: Menu  = new Menu();
+  communicationsMenu.name         = 'communications';
+  communicationsMenu.display      = 'Communications';
+  communicationsMenu.icon         = 'sensors';
+  communicationsMenu.subMenus     = subMenuCommunications(integrations);
   communicationsMenu.integrations = new Array<Integration>();
   integrations.forEach(i => {
     i.tags.forEach(t => {
@@ -321,12 +321,12 @@ function generateCommunications(integrations: Array<Integration>): Menu {
 }
 
 function subMenuCommunications(integrations: Array<Integration>): Array<SubMenu> {
-  const sm: Array<SubMenu> = new Array<SubMenu>();
+  const sm: Array<SubMenu>  = new Array<SubMenu>();
   // Caller ID
   const smCallerID: SubMenu = new SubMenu();
-  smCallerID.name = 'caller_id';
-  smCallerID.display = 'Caller ID';
-  smCallerID.integrations = new Array<Integration>();
+  smCallerID.name           = 'caller_id';
+  smCallerID.display        = 'Caller ID';
+  smCallerID.integrations   = new Array<Integration>();
   integrations.forEach(i => {
     i.tags.forEach(t => {
       if (smCallerID.name === t) {
@@ -341,11 +341,11 @@ function subMenuCommunications(integrations: Array<Integration>): Array<SubMenu>
 
 // PRODUCTIVITY
 function generateProductivity(integrations: Array<Integration>): Menu {
-  const productivityMenu: Menu = new Menu();
-  productivityMenu.name = 'productivity';
-  productivityMenu.display = 'Productivity';
-  productivityMenu.icon = 'bar_chart';
-  productivityMenu.subMenus = [];
+  const productivityMenu: Menu  = new Menu();
+  productivityMenu.name         = 'productivity';
+  productivityMenu.display      = 'Productivity';
+  productivityMenu.icon         = 'bar_chart';
+  productivityMenu.subMenus     = [];
   productivityMenu.integrations = new Array<Integration>();
   integrations.forEach(i => {
     i.tags.forEach(t => {
@@ -360,11 +360,11 @@ function generateProductivity(integrations: Array<Integration>): Menu {
 
 // HOTEL
 function generateHotel(integrations: Array<Integration>): Menu {
-  const hotelMenu: Menu = new Menu();
-  hotelMenu.name = 'hotel';
-  hotelMenu.display = 'Hotel';
-  hotelMenu.icon = 'hotel_class';
-  hotelMenu.subMenus = [];
+  const hotelMenu: Menu  = new Menu();
+  hotelMenu.name         = 'hotel';
+  hotelMenu.display      = 'Hotel';
+  hotelMenu.icon         = 'hotel_class';
+  hotelMenu.subMenus     = [];
   hotelMenu.integrations = new Array<Integration>();
   integrations.forEach(i => {
     i.tags.forEach(t => {
@@ -379,11 +379,11 @@ function generateHotel(integrations: Array<Integration>): Menu {
 
 // ENTERPRISE
 function generateEnterprise(integrations: Array<Integration>): Menu {
-  const enterpriseMenu: Menu = new Menu();
-  enterpriseMenu.name = 'enterprise';
-  enterpriseMenu.display = 'Enterprise';
-  enterpriseMenu.icon = 'business';
-  enterpriseMenu.subMenus = [];
+  const enterpriseMenu: Menu  = new Menu();
+  enterpriseMenu.name         = 'enterprise';
+  enterpriseMenu.display      = 'Enterprise';
+  enterpriseMenu.icon         = 'business';
+  enterpriseMenu.subMenus     = [];
   enterpriseMenu.integrations = new Array<Integration>();
   integrations.forEach(i => {
     i.tags.forEach(t => {
@@ -398,11 +398,11 @@ function generateEnterprise(integrations: Array<Integration>): Menu {
 
 // ACCOUNTING
 function generateAccounting(integrations: Array<Integration>): Menu {
-  const accountingMenu: Menu = new Menu();
-  accountingMenu.name = 'accounting';
-  accountingMenu.display = 'Accounting (i.e. QuickBooks)';
-  accountingMenu.icon = 'account_circle';
-  accountingMenu.subMenus = [];
+  const accountingMenu: Menu  = new Menu();
+  accountingMenu.name         = 'accounting';
+  accountingMenu.display      = 'Accounting (i.e. QuickBooks)';
+  accountingMenu.icon         = 'account_circle';
+  accountingMenu.subMenus     = [];
   accountingMenu.integrations = new Array<Integration>();
   integrations.forEach(i => {
     i.tags.forEach(t => {
